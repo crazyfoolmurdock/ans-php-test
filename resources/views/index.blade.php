c2adeb634bba012d01e324e0f8ac3106e8343c5f
asif rashid <asif.rashid@dclabs.co.uk>
2024-07-19 16:53:19 +0100

    form for search

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,13 @@
    
     <h1>Pokedex</h1>
  
+    <form method="get" action="/">
+        @csrf
+            <label for="name">Filter Pokemon</label>
+            <input type="text" name="name" id="name" required />
+            <button type="submit">Search</button>
+    </form>
+
      
     <h3>
      Available Pokemon
